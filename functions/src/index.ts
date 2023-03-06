7ca653489070c5c4ed3dc233ed6ab9fdd6d772c9
kazuki <kazuking.1911@gmail.com>
2023-03-06 23:01:41 +0900

    firestoreからデータ取れた

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,14 +1,29 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//     functions.logger.info("Hello logs!", { structuredData: true });
-//     response.send("Hello from Firebase!");
-// });
+type User = {
+    id: string;
+    login: string;
+    display_name: string;
+    type: string;
+    broadcaster_type: string;
+    description: string;
+    profile_image_url: string;
+    offline_image_url: string;
+    view_count: number;
+    created_at: string;
+}
 
 export const scheduledFunction = functions
     .region("asia-northeast1")
     .pubsub.schedule("0 0 * * *")
     .timeZone("Asia/Tokyo")
     .onRun(async () => {
+        admin.initializeApp({ credential: admin.credential.applicationDefault() });
+        const col = admin.firestore().collection("streamers") as admin.firestore.CollectionReference<User>;
+        const querySnapshot = await col.get();
+        const users = querySnapshot.docs.map((doc) => doc.data());
+
         console.log(new Date());
+        console.log(JSON.stringify(users));
     });

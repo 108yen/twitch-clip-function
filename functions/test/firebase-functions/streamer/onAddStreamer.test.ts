6dd8a5525532cee4f4fdcfeb0904a2aca8a7a8ab
kazuki <kazuking.1911@gmail.com>
2023-07-20 22:20:56 +0900

    test後の後処理追加

--- functions/test/firebase-functions/streamer/onAddStreamer.test.ts
+++ functions/test/firebase-functions/streamer/onAddStreamer.test.ts
@@ -7,6 +7,9 @@ import { Change } from 'firebase-functions/v2/firestore';
 import { testEnv } from '~/test/setUp';
 import { StreamerRepository } from '~/src/repositories/streamer';
 import { ClipRepository } from '~/src/repositories/clip';
+import { clipDocRef } from '~/src/firestore-refs/clipRefs';
+import { streamersDocRef } from '~/src/firestore-refs/streamerRefs';
+import { FieldValue } from 'firebase-admin/firestore';
 
 describe('onAddStreamerのテスト', () => {
     let wrappedOnAddStreamer: WrappedScheduledFunction | WrappedFunction<Change<QueryDocumentSnapshot>>;
@@ -16,11 +19,12 @@ describe('onAddStreamerのテスト', () => {
 
     test('新しいストリーマーのloginが追加されると、twitch apiからそのストリーマーの情報を取得し、streamersに格納される', async () => {
         const path = "streamers/new";
+        const testLogin = "surugamonkey0113";
         const beforeSnap = testEnv.firestore.makeDocumentSnapshot({
             logins: []
         }, path);
         const afterSnap = testEnv.firestore.makeDocumentSnapshot({
-            logins: ["surugamonkey0113"]
+            logins: [testLogin]
         }, path);
         const change = testEnv.makeChange(beforeSnap, afterSnap);
 
@@ -28,9 +32,15 @@ describe('onAddStreamerのテスト', () => {
         const streamerRepository = new StreamerRepository();
         const clipRepository = new ClipRepository();
         const streamers = await streamerRepository.fetchFirestoreStreamers();
-        const newStreamer = streamers?.find(e => e.login == "surugamonkey0113");
+        const newStreamer = streamers?.find(e => e.login == testLogin);
         expect(newStreamer).toBeDefined();
         expect(newStreamer?.id).toBeDefined();
         expect(await clipRepository.fetchClip(newStreamer!.id)).not.toThrowError;
+
+        //後処理
+        await clipDocRef({ clipId: newStreamer!.id }).delete();
+        await streamersDocRef.update({
+            streamers: FieldValue.arrayRemove(testLogin)
+        });
     })
 })

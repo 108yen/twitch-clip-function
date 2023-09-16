0dd5299a6791be728b5dbdceaa9abc07d06e6bcc
kazuki <kazuking.1911@gmail.com>
2023-09-16 15:03:03 +0900

    ロジックを分離

--- functions/src/firebase-functions/streamer/streamerSelection.ts
+++ functions/src/firebase-functions/streamer/streamerSelection.ts
@@ -16,28 +16,62 @@ export const streamerSelection = functions
     .pubsub.schedule(`30 5,11,17,23 * * *`)
     .timeZone(`Asia/Tokyo`)
     .onRun(async () => {
-        const streamerRepository = new StreamerRepository();
-        const clipRepository = new ClipRepository();
-        const fetchStreamers = await streamerRepository.getStreamers();
-        const oldStreamerIds = fetchStreamers.map(streamer => streamer.id);
-        //get twitch api token
-        const twitchStreamerApi = await TwitchStreamerApi.init(
-            process.env.TWITCH_CLIENT_ID!,
-            process.env.TWITCH_CLIENT_SECRET!
-        );
+        const findoutNewStreamer = await FindOutNewStreamer.init();
 
         /* ==================================
         Update existing streamer information
         ================================== */
-        //get streamers info from twitch api
-        const oldStreamers = await storeFolloweres(oldStreamerIds, twitchStreamerApi);
+        const oldStreamers = await findoutNewStreamer.getOldStreamer();
+        const oldStreamerIds = oldStreamers.map(e => e.id);
 
         /* ==================================
         Find out new streamer
         ================================== */
+        const stream = await findoutNewStreamer.getJpLiveStreaming();
+        const newStreamerIds = findoutNewStreamer.filterStreams(stream, oldStreamerIds);
+        const newStreamers = await findoutNewStreamer.getNewStreamerFollower(newStreamerIds);
+        const { selectedStreamers, removedStreamerIds, addedStreamerIds } = findoutNewStreamer
+            .concatAndFilter(oldStreamers, newStreamers);
+        const storedStreamers = await findoutNewStreamer.updateStreamerInfo(selectedStreamers);
+
+
+        /* ==================================
+        Add, delete clipDoc
+        ================================== */
+        await findoutNewStreamer.updateFirestore(storedStreamers, removedStreamerIds, addedStreamerIds);
+
+        functions.logger.info(`add ${addedStreamerIds.length}, delete ${removedStreamerIds.length} (total:${storedStreamers.length})`);
+    });
+
+class FindOutNewStreamer {
+    streamerRepository = new StreamerRepository();
+    clipRepository = new ClipRepository();
+    twitchStreamerApi: TwitchStreamerApi;
+    constructor(twitchStreamerApi: TwitchStreamerApi) {
+        this.twitchStreamerApi = twitchStreamerApi;
+    }
+
+    public static async init() {
+        const twitchStreamerApi = await TwitchStreamerApi.init(
+            process.env.TWITCH_CLIENT_ID!,
+            process.env.TWITCH_CLIENT_SECRET!
+        );
+        return new FindOutNewStreamer(twitchStreamerApi);
+    }
+
+    async getOldStreamer(): Promise<Array<Streamer>> {
+        const fetchStreamers = await this.streamerRepository.getStreamers();
+        const oldStreamerIds = fetchStreamers.map(streamer => streamer.id);
+        const oldStreamers = await storeFolloweres(oldStreamerIds, this.twitchStreamerApi);
+        return oldStreamers;
+    }
+    async getJpLiveStreaming(): Promise<Array<Stream>> {
+        const streams = await this.twitchStreamerApi.getJpStreams();
+        return streams;
+    }
+    filterStreams(streams: Array<Stream>, oldStreamerIds: Array<string>): Array<string> {
         const removeTag = [`ASMR`, `Commissions`];
         const removeId = [`126482446`, `9504944`];
-        const streams = await twitchStreamerApi.getJpStreams();
         const newStreamerIds = streams
             .filter(stream => {
                 if (stream.viewer_count == undefined || stream.user_id == undefined) {
@@ -61,14 +95,32 @@ export const streamerSelection = functions
                 return true;
             })
             .map(e => e.user_id!);
-        //get followe num, for each streamers
-        const newStreamers = await storeFolloweres(newStreamerIds, twitchStreamerApi);
+        return newStreamerIds;
+    }
+    async getNewStreamerFollower(newStreamerIds: Array<string>): Promise<Array<Streamer>> {
+        const newStreamers = await storeFolloweres(newStreamerIds, this.twitchStreamerApi);
+        return newStreamers;
+    }
+    concatAndFilter(oldStreamers: Array<Streamer>, newStreamers: Array<Streamer>) {
         //Select streamers with top 200 followers
         const sumStreamers = sortByFollowerNum(oldStreamers.concat(newStreamers));
         const selectedStreamers = sumStreamers.slice(0, 200);
         const selectedStreamerIds = selectedStreamers.map(e => e.id);
+        const newStreamerIds = newStreamers.map(e => e.id);
+        const removedStreamerIds = sumStreamers
+            .slice(200)
+            .map(e => e.id)
+            .filter(id => newStreamerIds.indexOf(id) == -1);
+        const addedStreamerIds = selectedStreamerIds
+            .filter(id => newStreamerIds.indexOf(id) != -1);
+        return { selectedStreamers, removedStreamerIds, addedStreamerIds };
+    }
+    async updateStreamerInfo(
+        selectedStreamers: Array<Streamer>
+    ): Promise<Array<Streamer>> {
+        const selectedStreamerIds = selectedStreamers.map(e => e.id);
         //push to firestore
-        const storedStreamers = await twitchStreamerApi
+        const storedStreamers = await this.twitchStreamerApi
             .getStreamers(selectedStreamerIds);
         //Re-enter the number of followers
         for (const key in storedStreamers) {
@@ -76,32 +128,23 @@ export const streamerSelection = functions
                 .find(e => e.id == storedStreamers[key].id);
             storedStreamers[key].follower_num = streamerInFollowerNum?.follower_num;
         }
-        await streamerRepository.updateStreamers(sortByFollowerNum(storedStreamers));
+        return storedStreamers;
+    }
+    async updateFirestore(
+        storedStreamers: Array<Streamer>,
+        removedStreamerIds: Array<string>,
+        addedStreamerIds: Array<string>
+    ) {
 
-
-        /* ==================================
-        Add, delete clipDoc
-        ================================== */
-
-        //If it had been deleted this time, delete clipDoc
-        const removedStreamerIds = sumStreamers
-            .slice(200)
-            .map(e => e.id)
-            .filter(id => newStreamerIds.indexOf(id) == -1);
+        await this.streamerRepository.updateStreamers(sortByFollowerNum(storedStreamers));
         for (const key in removedStreamerIds) {
-            await clipRepository.deleteClipDoc(removedStreamerIds[key]);
+            await this.clipRepository.deleteClipDoc(removedStreamerIds[key]);
         }
-
-        //If it had been added this time, create clipDoc
-        //Filter the elements held by both arrays
-        const addedStreamerIds = selectedStreamerIds
-            .filter(id => newStreamerIds.indexOf(id) != -1);
         for (const key in addedStreamerIds) {
-            const id = addedStreamerIds[key];
-            await clipRepository.createClipDoc(id);
+            await this.clipRepository.createClipDoc(addedStreamerIds[key]);
         }
-        functions.logger.info(`add ${addedStreamerIds.length}, delete ${removedStreamerIds.length} (total:${selectedStreamerIds.length})`);
-    });
+    }
+}
 
 async function storeFolloweres(ids: Array<string>, twitchStreamerApi: TwitchStreamerApi): Promise<Array<Streamer>> {
     const streamers: Array<Streamer> = [];

c7d37441b58f60e933fdc77883a49e22a12a708a
kazuki <kazuking.1911@gmail.com>
2023-09-17 00:54:30 +0900

    ClipRepositoryのエラー処理のリファクタリング

--- functions/src/repositories/clip.ts
+++ functions/src/repositories/clip.ts
@@ -5,52 +5,51 @@ import { clipDocRef } from "../firestore-refs/clipRefs";
 export class ClipRepository {
 
     async getClip(clipId: string): Promise<ClipDoc> {
-        const ds = await clipDocRef({ clipId: clipId }).get();
-        if (!ds.data()) {
-            throw new Error(`documentId: ${clipId}のclipの取得に失敗しました。`);
+        const ds = await clipDocRef({ clipId: clipId })
+            .get()
+            .catch((error) => {
+                functions.logger.error(`ClipRepository/getClip/clipDocRef.get(): ${error}`);
+                throw new Error(error);
+            });
+        if (!ds?.data()) {
+            throw new Error(`ClipRepository/getClip: ds.data is undefind, documentId: ${clipId}`);
         }
 
         return ds.data()!;
     }
 
     async updateClip(clipId: string, clipDoc: ClipDoc) {
-        try {
-            await clipDocRef({ clipId: clipId })
-                .set(clipDoc, { merge: true });
-        } catch (error) {
-            functions.logger.error(`${clipId}のclip情報の更新に失敗しました: ${error}`);
-        }
+        await clipDocRef({ clipId: clipId })
+            .set(clipDoc, { merge: true })
+            .catch((error) => {
+                functions.logger.error(`ClipRepository/updateClip/clipDocRef.set():${error}`)
+                throw new Error(error);
+            });
     }
 
     async createClipDoc(clipId: string) {
-        try {
-            await clipDocRef({ clipId: clipId }).set(new ClipDoc());
-        } catch (error) {
-            functions.logger.error(`docId:${clipId}の作成に失敗しました: ${error}`);
-        }
+        await clipDocRef({ clipId: clipId })
+            .set(new ClipDoc())
+            .catch((error) => {
+                functions.logger.error(`ClipRepository/createClipDoc/clipDocRef.set():${error}`)
+                throw new Error(error);
+            });
     }
-    
+
     batchCreateClipDoc(clipId: string, batch: FirebaseFirestore.WriteBatch) {
-        try {
-            batch.set(clipDocRef({ clipId: clipId }),new ClipDoc());
-        } catch (error) {
-            functions.logger.error(`docId:${clipId}の作成に失敗しました: ${error}`);
-        }
+        batch.set(clipDocRef({ clipId: clipId }), new ClipDoc());
     }
 
     async deleteClipDoc(clipId: string) {
-        try {
-            await clipDocRef({ clipId: clipId }).delete();
-        } catch (error) {
-            functions.logger.error(`docId:${clipId}の削除に失敗しました: ${error}`);
-        }
+        await clipDocRef({ clipId: clipId })
+            .delete()
+            .catch((error) => {
+                functions.logger.error(`ClipRepository/createClipDoc/clipDocRef.delete():${error}`)
+                throw new Error(error);
+            });
     }
 
     batchDeleteClipDoc(clipId: string, batch: FirebaseFirestore.WriteBatch) {
-        try {
-            batch.delete(clipDocRef({ clipId: clipId }));
-        } catch (error) {
-            functions.logger.error(`docId:${clipId}の削除に失敗しました: ${error}`);
-        }
+        batch.delete(clipDocRef({ clipId: clipId }));
     }
 }

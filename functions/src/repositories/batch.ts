aa62f8043c9c9c3040f4a60c4f18d1631b00905b
kazuki <kazuking.1911@gmail.com>
2023-09-17 00:56:52 +0900

    BatchRepositoryのリファクタリング

--- functions/src/repositories/batch.ts
+++ functions/src/repositories/batch.ts
@@ -1,21 +1,15 @@
 import { db } from "../firestore-refs/db";
 import * as functions from "firebase-functions";
 
-export class BatchRepository{
+export class BatchRepository {
     private batch: FirebaseFirestore.WriteBatch;
     private chunck = 0;
     constructor() {
         this.batch = db.batch();
     }
     async getBatch() {
-        if (this.chunck>400) {
-            this.chunck = 0;
-            await this.batch
-                .commit()
-                .catch((error) => {
-                    functions.logger.error(`BatchRepository/getBatch/this.batch.commit():${error}`)
-                    throw new Error(error);
-                });
+        if (this.chunck > 400) {
+            await this.commitBatch();
         }
         this.chunck++;
         return this.batch;
@@ -28,5 +22,6 @@ export class BatchRepository{
                 functions.logger.error(`BatchRepository/commitBatch/this.batch.commit():${error}`)
                 throw new Error(error);
             });
+        this.batch = db.batch();
     }
 }

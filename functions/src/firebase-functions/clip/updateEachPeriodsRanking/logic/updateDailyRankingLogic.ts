3115b889560e4ea790cf463cec68193d1d9953d2
kazuki <kazuking.1911@gmail.com>
2023-10-04 20:17:50 +0900

    updateDailyRankingLogic実装

--- functions/src/firebase-functions/clip/updateEachPeriodsRanking/logic/updateDailyRankingLogic.ts
+++ functions/src/firebase-functions/clip/updateEachPeriodsRanking/logic/updateDailyRankingLogic.ts
@@ -1,3 +1,52 @@
-export class UpdateDailyRankingLogic{
-    
+import assert from "assert"
+
+import { Clip } from "../../../../models/clip"
+import { ClipDoc } from "../../../../models/clipDoc"
+import { ClipRepository } from "../../../../repositories/clip"
+
+export class UpdateDailyRankingLogic {
+    clipRepository = new ClipRepository()
+
+    clipDoc: ClipDoc
+    constructor(clipDoc: ClipDoc) {
+        this.clipDoc = clipDoc
+    }
+
+    extractDayClips(): Array<Clip> {
+        const clips = this.clipDoc.clipsMap.get(`day`)
+        assert(typeof clips !== `undefined`)
+        return clips
+    }
+
+    async update() {
+        const clipDoc = await this.clipRepository.getClip(`daily`)
+        const newClips = this.extractDayClips()
+
+        const today = new Date()
+        const lastDay = new Date(today.getTime() - 24 * 60 * 60 * 1000)
+        clipDoc.clipsMap.set(`${lastDay.getMonth() + 1}/${lastDay.getDate()}`, newClips)
+
+        if (clipDoc.clipsMap.size > 7) {
+            const clipDocKeys = Array.from(clipDoc.clipsMap.keys())
+            const delKeys = clipDocKeys.sort(this.compareDates).slice(0,-7)
+            for (const key of delKeys) {
+                clipDoc.clipsMap.delete(key)
+            }
+        }
+
+        await this.clipRepository.updateClip(`daily`, clipDoc)
+    }
+
+    private compareDates(date1: string, date2: string): number {
+        const [month1, day1] = date1.split(`/`).map(Number)
+        const [month2, day2] = date2.split(`/`).map(Number)
+
+        if (month1 < month2) return -1
+        if (month1 > month2) return 1
+
+        if (day1 < day2) return -1
+        if (day1 > day2) return 1
+
+        return 0
+    }
 }
--- functions/test/firebase-functions/clip/updateEachPeriodsRanking/logic/updateDailyRankingLogic.test.ts
+++ functions/test/firebase-functions/clip/updateEachPeriodsRanking/logic/updateDailyRankingLogic.test.ts
@@ -1,36 +1,85 @@
 import { faker } from "@faker-js/faker"
 
+import { UpdateDailyRankingLogic } from "../../../../../src/firebase-functions/clip/updateEachPeriodsRanking/logic/updateDailyRankingLogic"
 import { Clip } from "../../../../../src/models/clip"
 import { ClipDoc } from "../../../../../src/models/clipDoc"
 import { ClipRepository } from "../../../../../src/repositories/clip"
 import { clipElementCheck, clipOrderCheck } from "../../checkFunctions"
 
 describe(`UpdateDailyRankingLogicのテスト`, () => {
-    const clipRepository = new ClipRepository()
-    beforeAll(async () => {
-        const clipDoc = createDammyData()
-        await clipRepository.updateClip(`daily`, clipDoc)
-    })
-    afterAll(async () => {
-        await clipRepository.deleteClipDoc(`daily`)
+    let todayClips: Array<Clip>
+    beforeAll(() => {
+        const today = new Date()
+        const lastDay = new Date(today.getTime() - 24 * 60 * 60 * 1000)
+        todayClips = createClipsData(lastDay, today)
     })
     afterEach(() => jest.restoreAllMocks())
     test(`ダミーデータがちゃんと作成出来ているかの確認`, async () => {
-        const clipDoc = await clipRepository.getClip(`daily`)
+        const clipDoc = await createDammyData()
         for (const [, clips] of clipDoc.clipsMap) {
             clipOrderCheck(clips)
             clipElementCheck(clips)
         }
     })
-    test.todo(`0じの実行だった場合、clipDocを受け取ってdayランキングだけ出す`)
-    test.todo(`firestoreの今のdayランキングをとって入れなおす`)
-    test.todo(`7日から漏れたやつは消す`)
+    test(`0じの実行だった場合、clipDocを受け取ってdayランキングだけ出す`, async () => {
+        const todayClipDoc = new ClipDoc({ clipsMap: new Map([[`day`, todayClips]]) })
+
+        const updateDailyRankingLogic = new UpdateDailyRankingLogic(todayClipDoc)
+        const clips = updateDailyRankingLogic[`extractDayClips`]()
+
+        expect(clips).toEqual(todayClips)
+    })
+    test(`firestoreの今のdayランキングをとって入れなおす`, async () => {
+        const getClipsSpy = jest
+            .spyOn(ClipRepository.prototype, `getClip`)
+            .mockImplementation(createDammyData)
+        const updateClipsSpy = jest
+            .spyOn(ClipRepository.prototype, `updateClip`)
+            .mockImplementation()
+
+        const todayClipDoc = new ClipDoc({
+            clipsMap: new Map([[`day`, todayClips]])
+        })
+
+        const updateDailyRankingLogic = new UpdateDailyRankingLogic(todayClipDoc)
+        await expect(updateDailyRankingLogic[`update`]()).resolves.not.toThrowError()
+
+        expect(getClipsSpy).toHaveBeenCalledTimes(1)
+        expect(updateClipsSpy).toHaveBeenCalledTimes(1)
+        expect(updateClipsSpy.mock.calls[0][0]).toEqual(`daily`)
+        const today = new Date()
+        const expectedKeys = [...Array(7).keys()].map((index) => {
+            const started_at = new Date(
+                today.getTime() - (index + 1) * 24 * 60 * 60 * 1000
+            )
+            return `${started_at.getMonth() + 1}/${started_at.getDate()}`
+        })
+        expect(
+            Array.from(updateClipsSpy.mock.calls[0][1].clipsMap.keys()).sort()
+        ).toEqual(expectedKeys.sort())
+        for (const [, clips] of updateClipsSpy.mock.calls[0][1].clipsMap) {
+            clipOrderCheck(clips)
+            clipElementCheck(clips)
+        }
+    })
+    test(`compareDatesのテスト`, () => {
+        const todayClipDoc = new ClipDoc({
+            clipsMap: new Map([[`day`, todayClips]])
+        })
+
+        const updateDailyRankingLogic = new UpdateDailyRankingLogic(todayClipDoc)
+
+        expect(updateDailyRankingLogic[`compareDates`](`10/10`, `10/9`)).toEqual(1)
+        expect(updateDailyRankingLogic[`compareDates`](`10/9`, `10/10`)).toEqual(-1)
+        expect(updateDailyRankingLogic[`compareDates`](`10/9`, `9/10`)).toEqual(1)
+        expect(updateDailyRankingLogic[`compareDates`](`9/9`, `10/10`)).toEqual(-1)
+    })
 })
 
-function createDammyData() {
+async function createDammyData() {
     const clipDoc = new ClipDoc()
     const today = new Date()
-    for (let index = 0; index < 7; index++) {
+    for (let index = 1; index < 8; index++) {
         const ended_at = new Date(today.getTime() - index * 24 * 60 * 60 * 1000)
         const started_at = new Date(ended_at.getTime() - 24 * 60 * 60 * 1000)
         const clips = createClipsData(started_at, ended_at)
@@ -39,7 +88,6 @@ function createDammyData() {
             clips
         )
     }
-    clipDoc.sort()
     return clipDoc
 }
 
@@ -77,6 +125,15 @@ function createClipsData(started_at?: Date, ended_at?: Date) {
             broadcaster_follower_num: faker.number.int(),
             broadcaster_login: faker.person.fullName()
         }
+    })
+    clips.sort((a, b) => {
+        if (!a.view_count) {
+            return 1
+        }
+        if (!b.view_count) {
+            return -1
+        }
+        return b.view_count - a.view_count
     })
     return clips
 }

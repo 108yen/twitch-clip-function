3513d7e2195eb1881ac22e3c4973bb8e1aee009c
kazuki <kazuking.1911@gmail.com>
2023-12-10 15:07:48 +0900

    使ってない奴をコメントアウト

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import * as admin from "firebase-admin"
 
 //deploy function
-import { updateAllRanking } from "./firebase-functions/clip/updateEachPeriodsRanking/updateAllRanking"
-import { updateDayRanking } from "./firebase-functions/clip/updateEachPeriodsRanking/updateDayRanking"
-import { updateMonthRanking } from "./firebase-functions/clip/updateEachPeriodsRanking/updateMonthRanking"
-import { updateWeekRanking } from "./firebase-functions/clip/updateEachPeriodsRanking/updateWeekRanking"
+// import { updateAllRanking } from "./firebase-functions/clip/updateEachPeriodsRanking/updateAllRanking"
+// import { updateDayRanking } from "./firebase-functions/clip/updateEachPeriodsRanking/updateDayRanking"
+// import { updateMonthRanking } from "./firebase-functions/clip/updateEachPeriodsRanking/updateMonthRanking"
+// import { updateWeekRanking } from "./firebase-functions/clip/updateEachPeriodsRanking/updateWeekRanking"
 import { updateYearRanking } from "./firebase-functions/clip/updateEachPeriodsRanking/updateYearRanking"
-import { updatePastRanking } from "./firebase-functions/clip/updatePastRanking"
-import { streamerSelection } from "./firebase-functions/streamer/streamerSelection"
+// import { updatePastRanking } from "./firebase-functions/clip/updatePastRanking"
+// import { streamerSelection } from "./firebase-functions/streamer/streamerSelection"
 
 async function main() {
     // Firebase Admin SDK の初期化

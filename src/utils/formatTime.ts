d26ca157a616c4233e9dbeaad7b90bc7dfad5d01
kazuki <kazuking.1911@gmail.com>
2024-01-05 17:01:02 +0900

    formatTimeの実装変更とテスト作成

--- src/utils/formatTime.ts
+++ src/utils/formatTime.ts
@@ -6,8 +6,17 @@ export function formatTime(milliseconds: number) {
     return `${hours}hours ${minutes % 60}minutes ${seconds % 60}seconds`
 }
 
-export function formatDate(date: Date) {
+export function getJSTHours() {
+    return getJSTDate().getUTCHours()
+}
+
+export function getJSTDate() {
+    return setJSTDateInUTCDate(new Date())
+}
+
+function setJSTDateInUTCDate(date: Date) {
     const options: Intl.DateTimeFormatOptions = {
+        timeZone: `Asia/Tokyo`,
         year: `numeric`,
         month: `2-digit`,
         day: `2-digit`,
@@ -16,32 +25,8 @@ export function formatDate(date: Date) {
         second: `2-digit`
     }
 
-    const formattedDate = new Intl.DateTimeFormat(undefined, options).format(date)
-    return formattedDate
-}
+    const formattedDate = new Intl.DateTimeFormat(`ja-JP`, options).format(date)
 
-// export function getJSTHours() {
-//     const date = new Date()
-//     const hour = date.getUTCHours()
-//     return hour + 9 < 24 ? hour + 9 : hour - 15
-// }
-
-// export function getJSTDate() {
-//     const date = new Date()
-//     const time = date.getTime()
-//     return new Date(time + 9 * 60 * 60 * 1000)
-// }
-
-export function getJSTHours() {
-    return getJSTDate().getHours()
-}
-
-export function getJSTDate() {
-    return utcToJst(new Date())
-}
-
-function utcToJst(date: Date) {
-    const jstFormatter = new Intl.DateTimeFormat(`ja-JP`, { timeZone: `Asia/Tokyo` })
-    const jstTime = jstFormatter.format(date)
-    return new Date(jstTime)
+    //utcに無理やりJST時間をセットする
+    return new Date(Date.parse(`${formattedDate}Z`))
 }

62980bc12ad74c965df8de02d878d00770843763
mac <kazuking.1911@gmail.com>
2024-01-20 11:56:30 +0900

    backtickに変更

--- src/utils/dayjs.ts
+++ src/utils/dayjs.ts
@@ -2,7 +2,7 @@ import dayjs from 'dayjs'
 import "dayjs/locale/ja"
 import duration from "dayjs/plugin/duration"
 
-dayjs.locale('ja')
+dayjs.locale(`ja`)
 dayjs.extend(duration)
 
 export default dayjs
--- test/utils/dayjs.test.ts
+++ test/utils/dayjs.test.ts
@@ -1,11 +1,11 @@
 import dayjs from "../../src/utils/dayjs"
 
-describe('dayjsのテスト', () => {
-    test('localeの確認', () => {
+describe(`dayjsのテスト`, () => {
+    test(`localeの確認`, () => {
         const day = dayjs()
-        expect(day.locale).toEqual('ja')
+        expect(day.locale).toEqual(`ja`)
     })
-    test('時間の確認', () => {
+    test(`時間の確認`, () => {
         //dateで計算
         const date = new Date()
         const JSThour = date.getHours() + 9 > 23 ? date.getHours() - 15 : date.getHours() + 9;

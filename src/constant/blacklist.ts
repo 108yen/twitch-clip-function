835f3763ccc25a11f71aef1a9bf23c7a81a4031e
108yen <kazuking.1911@gmail.com>
2024-10-22 13:39:12 +0900

    chore: update blacklist

--- src/constant/blacklist.ts
+++ src/constant/blacklist.ts
@@ -1,16 +1,29 @@
 export const BLACKLIST = {
   IDs: [
-    `496970086`, //
-    `126482446`,
-    `9504944`,
-    `840446934`,
-    `208760543`,
-    `134850221`, //RTAinJapan
-    `182565961`, //World of Warships
-    `179988448`, //PUBG JAPAN
-    `144740532`, //japanese_stream
-    `229395457`, //east geeks mash
-    `79294007`, //mira
+    "496970086", //
+    "126482446",
+    "9504944",
+    "840446934",
+    "208760543",
+    "134850221", //RTAinJapan
+    "182565961", //World of Warships
+    "179988448", //PUBG JAPAN
+    "144740532", //japanese_stream
+    "229395457", //east geeks mash
+    "79294007", //mira
+    "544210045", // VALORANT jp
+    "104833324", // Riot
+    "620880640", // rage
+    "769243599", // crazyraccoon406
+    "563905034", // AmazonMusicJP
+    "150989534", // 冠緯
+    "147462900", // capcom
+    "38082778", // redbull
+    "882546504", // OW esports
+    "21701992", // TOPANGAtv
+    "557720142", // AmazonMusicUK
+    "971372287", // thek4sen
+    "238320465", // zetadivision
   ],
-  Tags: [`ASMR`, `Commissions`],
+  Tags: ["ASMR", "Commissions"],
 }

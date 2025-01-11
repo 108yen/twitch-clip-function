bd8edbeddcab283f342eeee760ade2638eface85
108yen <kazuking.1911@gmail.com>
2025-01-11 10:59:11 +0900

    test: updated twitch api tests

--- test/apies/clip.test.ts
+++ test/apies/clip.test.ts
@@ -5,46 +5,71 @@ import { Clip } from "../../src/models/clip"
 import dayjs from "../../src/utils/dayjs"
 
 describe(`TwitchClipApiのテスト`, () => {
+  const broadcasterId = 203654142 // ファン太
   let twitchClipApi: TwitchClipApi
+
   beforeAll(async () => {
     twitchClipApi = await TwitchClipApi.init(
       process.env.TWITCH_CLIENT_ID!,
       process.env.TWITCH_CLIENT_SECRET!,
     )
   })
+
   test(`getClips:期間指定`, async () => {
     const now = dayjs()
-    const daysAgo = now.subtract(1, `year`) //days ago
+    const daysAgo = now.subtract(1, `year`)
     const result: Array<Clip> = await twitchClipApi.getClips(
-      203654142,
+      broadcasterId,
       daysAgo,
       now,
     )
-    expect(result.length).toEqual(100)
-    for (const key in result) {
-      const clip = result[key]
-      expect(clip.title).toBeDefined()
-      expect(clip.view_count).toBeDefined()
-      expect(clip.created_at).toBeDefined()
-      const date = dayjs(clip.created_at!)
+
+    expect(result.length).toBeGreaterThan(90)
+
+    for (const clip of result) {
+      const {
+        broadcaster_name,
+        created_at,
+        creator_name,
+        embed_url,
+        title,
+        view_count,
+      } = clip
+
+      expect(title).toBeDefined()
+      expect(view_count).toBeDefined()
+      expect(created_at).toBeDefined()
+      expect(broadcaster_name).toBeDefined()
+      expect(embed_url).toBeDefined()
+      expect(creator_name).toBeDefined()
+
+      const date = dayjs(created_at!)
       expect(date.unix()).toBeGreaterThanOrEqual(daysAgo.unix())
       expect(date.unix()).toBeLessThanOrEqual(now.unix())
-      expect(clip.broadcaster_name).toBeDefined()
-      expect(clip.embed_url).toBeDefined()
-      expect(clip.creator_name).toBeDefined()
     }
   })
+
   test(`getClips:期間未指定`, async () => {
-    const result: Array<Clip> = await twitchClipApi.getClips(203654142)
-    expect(result.length).toEqual(100)
-    for (const key in result) {
-      const clip = result[key]
-      expect(clip.title).toBeDefined()
-      expect(clip.view_count).toBeDefined()
-      expect(clip.created_at).toBeDefined()
-      expect(clip.broadcaster_name).toBeDefined()
-      expect(clip.embed_url).toBeDefined()
-      expect(clip.creator_name).toBeDefined()
+    const result: Array<Clip> = await twitchClipApi.getClips(broadcasterId)
+
+    expect(result.length).toBeGreaterThan(90)
+
+    for (const clip of result) {
+      const {
+        broadcaster_name,
+        created_at,
+        creator_name,
+        embed_url,
+        title,
+        view_count,
+      } = clip
+
+      expect(title).toBeDefined()
+      expect(view_count).toBeDefined()
+      expect(created_at).toBeDefined()
+      expect(broadcaster_name).toBeDefined()
+      expect(embed_url).toBeDefined()
+      expect(creator_name).toBeDefined()
     }
   })
 })
